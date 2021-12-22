78576d63995445e774b549e831c284fa2c28e9bc
enochndika <enochbeloved@gmail.com>
2021-12-22 18:28:28 +0100

    first major version

---
+++ src/dashboard/Layout.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { browser } from '$app/env';
+	import { open } from './provider/store';
+	import Overlay from './provider/Overlay.svelte';
+	import TopNavigation from './topnavigation/Index.svelte';
+	import SideNavigation from './sidenavigation/Index.svelte';
+
+	const style = {
+		container: `bg-gray-100 h-screen overflow-hidden relative`,
+		main: `h-screen overflow-auto pb-36 pt-4 px-2 md:pb-8 lg:px-4`,
+		mainContainer: `flex flex-col h-screen pl-0 w-full lg:pl-24 lg:space-y-4`
+	};
+
+	if (browser) {
+		page.subscribe(() => {
+			// close side navigation when route changes
+			$open = false;
+		});
+	}
+
+	onMount(() => {
+		document.getElementsByTagName('body').item(0).removeAttribute('tabindex');
+	});
+</script>
+
+<div class={style.container}>
+	<div class="flex items-start">
+		<Overlay />
+		<SideNavigation mobilePosition="right" />
+		<div class={style.mainContainer}>
+			<TopNavigation />
+			<main class={style.main}>
+				<slot />
+			</main>
+		</div>
+	</div>
+</div>
---
+++ src/dashboard/sidenavigation/Items.svelte
@@ -0,0 +1,31 @@
+<script>
+	import data from './data';
+	import { page } from '$app/stores';
+	import { open } from '../provider/store';
+
+	const style = {
+		title: `mx-4 text-sm`,
+		inactive: `text-white`,
+		active: `font-medium text-green-400 hover:text-green-400`,
+		link: `flex items-center justify-start my-2 p-4 w-full hover:text-white`,
+		close: `lg:duration-700 lg:ease-out lg:invisible lg:opacity-0 lg:transition-all`,
+		open: `lg:duration-500 lg:ease-in lg:h-auto lg:opacity-100 lg:transition-all lg:w-auto`
+	};
+</script>
+
+<ul class="md:pl-5">
+	<li>
+		{#each data as item, i (item.title)}
+			<a
+				href={item.link}
+				class={`${style.link} 
+            ${item.link === $page.path ? style.active : style.inactive}`}
+			>
+				<span><svelte:component this={item.icon} /></span>
+				<span class={`${style.title} ${$open ? style.open : style.close}`}>
+					{item.title}
+				</span>
+			</a>
+		{/each}
+	</li>
+</ul>
