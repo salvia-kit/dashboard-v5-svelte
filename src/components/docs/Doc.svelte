e086898d5a207e28eb4c1824d2af584b1b4de5fc
enoch ndika <enochbeloved@gmail.com>
2022-02-26 21:23:10 +0100

    update deps and svelte config

--- src/components/docs/Doc.svelte
+++ src/components/docs/Doc.svelte
@@ -99,7 +99,7 @@
 				<FolderIcon /> <strong class="px-1">provider</strong>
 			</div>
 			<div class="flex flex-wrap md:flex-nowrap pl-6 mt-4">
-				<JsIcon class="h-7 w-7" />
+				<JsIcon extendClass="h-7 w-7" />
 				<span class="px-2 font-medium">click-outside.js</span> :
 				<p class="pl-2">it handles the click outside of sidenav.</p>
 			</div>
@@ -112,7 +112,7 @@
 				</p>
 			</div>
 			<div class="flex flex-wrap md:flex-nowrap pl-6 mt-6">
-				<JsIcon class="h-7 w-7" />
+				<JsIcon extendClass="h-7 w-7" />
 				<span class="px-2 font-medium">store.js</span> :
 				<p class="pl-2">contains the store value for toggling sidenav state</p>
 			</div>
@@ -123,7 +123,7 @@
 				<FolderIcon /> <strong class="px-1">sidenavigation</strong>
 			</div>
 			<div class="flex flex-wrap md:flex-nowrap pl-6 mt-4">
-				<FolderIcon class="h-7 w-7" />
+				<FolderIcon extendClass="h-7 w-7" />
 				<span class="px-2 font-medium">icons</span> :
 				<p class="pl-1">
 					As each <strong>sidenav-item</strong> has an icon, this sub-folder contains the icons of each
